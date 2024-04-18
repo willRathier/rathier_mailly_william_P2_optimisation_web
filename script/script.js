57647c8de08eea57735cb0c73ae688a3f337cda4
willRathier <willi02rm@gmail.com>
2024-04-18 17:50:15 -0400

    lot of changes part 2

--- script/script.js
+++ script/script.js
@@ -1,1 +1,7 @@
-gsap.to(".arrow", { y: "7vh", duration: 2, repeat: -1 });
+gsap.to(".arrow", { y: "7vh", duration: 1, repeat: -1, yoyo: true });
+
+let timer = setTimeout(100);
+
+window.addEventListener(scroll, function () {
+  console.log("Scroll détecté");
+});
